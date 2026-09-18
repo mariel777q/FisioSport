11e34edcd1287285fe0527f9bb4f9410d7b15750
mariel777q <apalamariel8@gmail.com>
2026-09-17 21:32:58 -0400

    Corregir ruta principal para mostrar landing

--- FISIOSPORT/Program.cs
+++ FISIOSPORT/Program.cs
@@ -261,7 +261,7 @@ app.MapGet("/health", () => Results.Ok("FisioSport OK"));
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}"
+    pattern: "{controller=Home}/{action=Index}/{id?}"
 );
 
 app.Run();
